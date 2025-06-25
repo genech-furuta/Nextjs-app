219c27d6f83438be6d3596568dbc5236d160e64b
genech-furuta <furutaa@genech.co.jp>
2025-06-25 11:20:57 +0900

    Add buildspec for CodeBuild

--- app/good/page.tsx
+++ app/good/page.tsx
@@ -1,7 +1,7 @@
 export default function GoodPage() {
   return (
     <main>
-      <h1>222good Page</h1>
+      <h1>22good Page</h1>
       <p>This is the about page of the app.</p>
     </main>
   );
